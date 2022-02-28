1a65b753995a12ca54f12d12d41e5ada7e2b4c9a
fuelcredit <repo@myfuelcredit.com>
2022-02-28 11:58:51 +0100

    Fixed error

--- app/Http/Controllers/Cashback/TransactionController.php
+++ app/Http/Controllers/Cashback/TransactionController.php
@@ -4,15 +4,21 @@ namespace App\Http\Controllers\Cashback;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class TransactionController extends Controller
 {
     public function store(Request $request)
     {
         // validate user input;
-        $data = $request->file('txn');
-        dd($data);
-        $transactions = json_decode($data);
+        $path = $request->file('txn');
+        $jsonString = file_get_contents($path);
+        $data = json_decode($jsonString, true);
+        print_r($data);
+
+        /* $jsonString = file_get_contents(base_path('public/json/N4Eje3JbsOXUhjypIXnvkH9tY7q1Rg7qfd4b0Beh.json'));
+        $data = json_decode($jsonString, true);
+        print_r($data); */
     }
 
     public function convertCsv()
